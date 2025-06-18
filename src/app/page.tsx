f4f03b95db9df32c27c5276a039277f698cdef21
Radek Kyselý <radek@kysely.org>
2025-06-19 00:09:55 +0200

    feat: Explicitly enable both Node & Edge functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 export default function Home() {
   return (
-    <a href="/api/query">Go to `/api/query`</a>
+    <ul>
+      <li>
+        <a href="/api/query/node">Go to `/api/query/node`</a>
+      </li>
+      <li>
+        <a href="/api/query/edge">Go to `/api/query/edge`</a>
+      </li>
+    </ul>
   );
 }
